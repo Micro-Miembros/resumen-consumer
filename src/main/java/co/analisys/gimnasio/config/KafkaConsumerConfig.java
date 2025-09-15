22207d26669a1f151f424cd48570753bad4289d7
Brayan Ortega <brayansteven0322@gmail.com>
2025-09-15 11:22:14 -0500

    update ports for docker

--- src/main/java/co/analisys/gimnasio/config/KafkaConsumerConfig.java
+++ src/main/java/co/analisys/gimnasio/config/KafkaConsumerConfig.java
@@ -21,7 +21,7 @@ public class KafkaConsumerConfig {
     @Bean
     public KafkaConsumer<String, ResumenEntrenamiento> resumenKafkaConsumer() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "resumen-consumer-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
